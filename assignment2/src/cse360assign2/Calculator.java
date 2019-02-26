1d031b994a558c27fbd460dcbe82dff36ce0f0f9
yusra <yusra@yusra>
2019-02-25 21:58:57 -0700

    Version 1

---
+++ assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,70 @@
+/**
+ * @author Yusra Hashmi
+ * Class ID: 399
+ * Assignment #2
+ * Description: This code functions as a calculator which can manipulate user input and return arithmetic results.
+ */
+package cse360assign2;
+
+
+/*
+ * The Calculator class takes an input of numbers and can add, subtract, multiply, and divide integers.
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * The Calculator() constructor defines an integer to keep track of the total value. 
+	 * The integer is initialized to 0. 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The getTotal() method returns the total calculated. 
+	 * @return int value of the total. 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * The add(int value) adds a value to the total value. 
+	 * @param value to be added 
+	 */
+	public void add (int value) {
+	}
+	
+	/**
+	 * The subtract(int value) method subtracts a value from the total value.
+	 * @param value to be subtracted
+	 */
+	public void subtract (int value) {
+	}
+	
+	/**
+	 *  The multiply(int value) method multiplies the total by the value.
+	 * @param value is the integer to multiply by
+	 */
+	public void multiply (int value) {
+	}
+	
+	/**
+	 * The divide(int value) method divides the total by the parameter value.
+	 * @param value is the number to divide total by.
+	 */
+	public void divide (int value) {
+	}
+	
+	/**
+	 * The getHistory() method returns the history of all actions. 
+	 * @return all previous actions/requests
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
+
